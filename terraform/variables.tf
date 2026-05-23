3a115ec73f30a47fa2c66e7a7eab30756333ed4d
Naoueldjouher <hellonaouel@gmail.com>
2026-05-23 21:01:57 +0200

    chore: remove legacy terraform platform files

--- terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "region" { default = "us-east-1" }
-variable "bucket_name" { type = string }
-variable "vpc_id" { type = string }
-variable "subnet_ids" { type = list(string) }
-variable "env_file_path" {
-  type        = string
-  description = "Path to generate the .env file"
-  default     = "./.env" 
-}
